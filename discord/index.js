c11811f1141cc244c7d582a2af3b4e3253988abb
eto56 <arimakishou0505@gmail.com>
2025-07-02 17:53:49 +0900

    fix index.js

--- discord/index.js
+++ discord/index.js
@@ -1,48 +1,45 @@
 const { Client, GatewayIntentBits } = require("discord.js");
-const { fetch } = require('undici');
-require('dotenv').config({ path: './../.env' });
+const { fetch } = require("undici");
+require("dotenv").config({ path: "./../.env" });
 const client = new Client({
   intents: [
-    GatewayIntentBits.Guilds,            // ギルド自体の接続を許可
-    GatewayIntentBits.GuildMessages,     // ギルド内のメッセージ受信を許可
-    GatewayIntentBits.MessageContent     // メッセージ内容の取得を許可
-  ]
+    GatewayIntentBits.Guilds, // ギルド自体の接続を許可
+    GatewayIntentBits.GuildMessages, // ギルド内のメッセージ受信を許可
+    GatewayIntentBits.MessageContent, // メッセージ内容の取得を許可
+  ],
 });
 
-
 const msghandler = async (msg) => {
- if (!msg.content.startsWith("!")) return;
- 
- const message = msg.content.slice(1);
- if (message == "help") {
-  await msg.reply("駅名を入力してください。例: !新宿");
-  return;
- }
+  if (!msg.content.startsWith("!")) return;
 
- const station = message;
-  const params = { station };
+  const message = msg.content.slice(1).split(" ");
+ 
+  if (message == "help" && message.length == 1) {
+    await msg.reply("駅名を入力してください。例: !新宿 居酒屋");
+    return;
+  }
+
+  const station = message[0];
+  const genre = message[1] || ""; // デフォルトは "all"
+  const params = { station, genre };
   console.log(params);
 
-      try {
-        const res = await fetch('http://backend:8080/search', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(params)
-        });
-        if (!res.ok) throw new Error('検索に失敗しました');
-        const data = await res.json();
-        const name = data.name
-        const url = data.url
+  try {
+    const res = await fetch("http://backend:8080/search", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(params),
+    });
+    if (!res.ok) throw new Error("検索に失敗しました");
+    const data = await res.json();
+    const name = data.name;
+    const url = data.url;
 
-        await msg.reply(`店名: ${name}\nURL: ${url}`);
-      } 
-      catch (err) {
-        await msg.reply("駅名を入力してください。例: !新宿");
-        
-      }
-}
-
- 
+    await msg.reply(`店名: ${name}\nURL: ${url}`);
+  } catch (err) {
+    await msg.reply("駅名を入力してください。例: !新宿");
+  }
+};
 
 client.on("ready", () => console.log("Logged in"));
 client.on("messageCreate", msghandler);
